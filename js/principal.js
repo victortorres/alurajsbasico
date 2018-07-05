c65eff425d0baba7bd90e95c5598f54d32921a1c
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-05 20:25:34 +0100

    adicionado validacoes e um loop para percorrer a lista de pacientes e fazendo o calculo do IMC

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,43 @@
+//estou buscando pela classe css
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+//estou buscando pelo id
+//se usar o querySelector, mesmo que tenha mais itens com a mesma class, ele
+//soh pega um, mas se usar o querySelectorAll ele retorna a lista
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    
+    if (peso <= 0 || peso >= 1000) {
+        tdImc.textContent = "Peso Inválido";
+        //<objeto>.style serve para alterar atributos manipulados por CSS
+        //para atributos com mais de um nome, usar o formado camel case
+        //<objeto>.classList.add(<nome-da-classe-css>) serve para adicionar
+        //uma nova classe css para a tag
+        paciente.classList.add("paciente-invalido");
+        //paciente.style.color = "white";
+        //paciente.style.backgroundColor = "red";
+    }
+
+    if (altura <= 0 || altura >= 3.0) {
+        tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    var imc = peso / (altura * altura);
+    
+    tdImc.textContent = imc.toFixed(2);
+
+}
+
+
+
