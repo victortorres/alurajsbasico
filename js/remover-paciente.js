aaac27ce47ebccc54e989e9b08b85d2631e3aa58
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-11 21:17:17 +0100

    corrigido bug de remocao para poder escutar o pai ao inves dos filhos da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,10 @@
-var pacientesTabela = document.querySelectorAll(".paciente");
+var pacientesTabela = document.querySelector("#tabela-pacientes");
 
-pacientesTabela.forEach(function(pacienteTr){
-    pacienteTr.addEventListener("dblclick", function(){
-        this.remove();
-    });
+pacientesTabela.addEventListener("dblclick", function(event){
+    //o listener foi adicionado no pai, que eh a tabela, mas
+    //o evento vai acontecer em um dos filhos, como a td ou a tr
+    //com esse comando eu consigo pegar quem de fato sofreu o evento
+    //e quando eu passo o parentNode eu estou pegado a tag pai de onde
+    //foi executado o evento
+    event.target.parentNode.remove();
 });
