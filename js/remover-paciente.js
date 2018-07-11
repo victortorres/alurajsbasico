82ba7727b4d709df7bdebdec5744171598d3a7d9
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-11 21:27:15 +0100

    adicionando um efeito de transicao ao remover linha da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,10 +1,15 @@
 var pacientesTabela = document.querySelector("#tabela-pacientes");
 
 pacientesTabela.addEventListener("dblclick", function(event){
-    //o listener foi adicionado no pai, que eh a tabela, mas
-    //o evento vai acontecer em um dos filhos, como a td ou a tr
-    //com esse comando eu consigo pegar quem de fato sofreu o evento
-    //e quando eu passo o parentNode eu estou pegado a tag pai de onde
-    //foi executado o evento
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    
+    setTimeout(function(){
+        //o listener foi adicionado no pai, que eh a tabela, mas
+        //o evento vai acontecer em um dos filhos, como a td ou a tr
+        //com esse comando eu consigo pegar quem de fato sofreu o evento
+        //e quando eu passo o parentNode eu estou pegado a tag pai de onde
+        //foi executado o evento
+        event.target.parentNode.remove();
+    }, 500);
+    
 });
